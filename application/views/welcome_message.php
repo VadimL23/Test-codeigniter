6488835697540b8d56888a16a4ce1050c0f40a34
Vadim <Vadim_lachkov@mail.ru>
2021-09-01 15:09:53 +0700

    create codeigniter3

---
+++ application/views/welcome_message.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Test with CodeIgniter</title>
+
+</head>
+<body>
+	<div id="container">
+	
+  <h1> Test codeigniter</h1>
+		<div id="body">
+
+		</div>
+    </div>
+
+</body>
+</html>
